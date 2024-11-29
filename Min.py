b4284022ea56a3c1989e54a1bf7e07e7fa3f6143
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-29 19:04:21 +0530

    ACP - 1

--- Min.py
+++ Min.py
@@ -7,17 +7,17 @@ turns = 5
 
 for i in range( 1 , 11):
   
-  i = int(input("Enter the number:"))
+  i = int(input("Enter the number: "))
 
   if (i==num):
     print("Congratulation you Guessed the number")
 
   elif i>num:
-    print("Number is greater")
+    print("Number is lesser")
     turns-=1
 
   elif i<num:
-    print("Number is lesser")
+    print("Number is greater")
     turns-=1
 
   if turns ==0:
